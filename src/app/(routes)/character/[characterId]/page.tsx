3adbfc7b06eeea81a1f903ec572f498f5ca0502d
Muse <musemy8484@gmail.com>
2025-11-29 17:15:24 +0300

    fix: bug fixed

--- src/app/(routes)/character/[characterId]/page.tsx
+++ src/app/(routes)/character/[characterId]/page.tsx
@@ -3,13 +3,55 @@ import CharacterForm from "../_components/character-form";
 import { auth } from "@clerk/nextjs/server";
 import { Metadata } from "next";
 
-type CharacterPageProps = {
-  params: {
-    characterId: string;
-  };
-};
+type Params = Promise<{
+  characterId: string;
+}>;
 
-const CharacterIdPage = async ({ params }: CharacterPageProps) => {
+export async function generateMetadata({
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const { characterId } = await params;
+  const character = await db.character.findUnique({
+    where: {
+      id: characterId,
+    },
+    include: {
+      category: true,
+    },
+  });
+  if (!character) {
+    return {
+      title: "Character Not Found",
+      description: "Character Not Found",
+    };
+  }
+  return {
+    title: `${character?.name}`,
+    keywords: [
+      `${character?.name} ai companion,${character?.name} ai Chatbot,${character?.name} ai, ${character?.name},${character?.category.name} ai companion,${character?.category.name} ai chatbot`,
+    ],
+    openGraph: {
+      title: `${character?.name} AI companion`,
+      description: `Edit ${character?.name} AI companion`,
+      url: "https://ai-companions-alpha.vercel.app",
+      images: [
+        {
+          url: character?.imageUrl as string,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${character?.name} AI companion`,
+      description: `Edit ${character?.name} AI companion`,
+      images: [`${character?.imageUrl as string}`],
+    },
+  };
+}
+
+const CharacterIdPage = async ({ params }: { params: Params }) => {
   const { userId, redirectToSignIn } = await auth();
   const { characterId } = await params;
 
